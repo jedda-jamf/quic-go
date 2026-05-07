360c13bda0f62ab9c0cfde24f63a1e1ae1d68c7b
Jedda Wignall <jedda.wignall@jamf.com>
2026-05-07 14:04:10 +1000

    test(bbr): remove old gain test superseded by table-driven version

--- internal/congestion/bbr_v3_test.go
+++ internal/congestion/bbr_v3_test.go
@@ -1792,26 +1792,6 @@ func TestBBRv3AckAggregationRaisesCwndTarget(t *testing.T) {
 	require.Greater(t, withExtraAcked, withoutExtraAcked)
 }
 
-func TestBBRv3RFCGains(t *testing.T) {
-	bbr := newTestBBRv3()
-
-	bbr.state = BBRDrain
-	bbr.updateGains()
-	require.InDelta(t, 0.5, bbr.pacingGain, 0.0001)
-	require.InDelta(t, 2.0, bbr.cwndGain, 0.0001)
-
-	bbr.state = BBRProbeBW
-	bbr.probeBWPhase = probeBWDown
-	bbr.updateGains()
-	require.InDelta(t, 0.9, bbr.pacingGain, 0.0001)
-	require.InDelta(t, 2.0, bbr.cwndGain, 0.0001)
-
-	bbr.state = BBRProbeRTT
-	bbr.updateGains()
-	require.InDelta(t, 1.0, bbr.pacingGain, 0.0001)
-	require.InDelta(t, PROBE_RTT_CWND_GAIN, bbr.cwndGain, 0.0001)
-}
-
 func TestBBRv3ProbeRTTCwndUsesBoundedBandwidth(t *testing.T) {
 	bbr := newTestBBRv3()
 	bbr.minRTT = 40 * time.Millisecond
